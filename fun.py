0a3009a8dc8d3a33762c033c1e494219757dd2d1
Alice Chen <alice@perka.com>
2017-12-17 23:30:28 -0500

    fix

--- fun.py
+++ fun.py
@@ -1,14 +1,16 @@
 import math
 
 def spiral_memory(num):
+	if num < 2:
+		return 0
 	square_rooted = math.floor(math.sqrt(num))
-	next_odd_squre = (square_rooted + 1) if square_rooted % 2 is 0 else (square_rooted + 2)
-	previous_odd = next_odd_squre - 2
-	ring_dist = math.floor(next_odd_squre / 2)
+	next_odd_square = (square_rooted + 1) if square_rooted % 2 < 1 else (square_rooted + 2)
+	previous_odd = next_odd_square - 2
+	ring_dist = math.floor(next_odd_square / 2)
 	dist = (num - previous_odd ** 2) % (previous_odd + 1)
  	row_distnce = abs(dist - ring_dist)
 	steps = row_distnce + ring_dist
 	return steps
 
-# print(spiral_memory(277678))
+print(spiral_memory(57))
 
