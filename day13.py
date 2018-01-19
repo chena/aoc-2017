c4b01d5731d51f4f3e45ccc70484fd69cb68d0d8
Alice Chen <alice@perka.com>
2018-01-19 08:18:31 -0500

    Day 13 part 2 wip

--- day13.py
+++ day13.py
@@ -1,7 +1,42 @@
+from copy import deepcopy
 """
 http://adventofcode.com/2017/day/13
 """
-def packet_scanner(file='input/packets.txt'):
+def __move_packets__(packets):
+  for rr in packets.values():
+    if rr[2]:
+      rr[0] -=1
+    else:
+      rr[0] += 1
+    if rr[0] == 0 or rr[0] == rr[1] - 1:
+      rr[2] = not rr[2]
+  # print packets
+
+def packet_scanner(packets, max_depth):  
+  severity = 0
+  for d in range(max_depth + 1):
+    if d in packets:
+      r =  packets[d]
+      if r[0] == 0:
+        severity += d * r[1]
+    __move_packets__(packets)
+  return severity
+
+def packet_scan_check(packets, max_depth, delay=0):
+  for n in range(delay):
+    __move_packets__(packets)
+
+  caught = False
+  for d in range(max_depth + 1):
+    if d in packets:
+      r =  packets[d]
+      if r[0] == 0:
+        caught = True
+        break
+    __move_packets__(packets)
+  return caught
+
+def packet_scanner_delay(file='input/packets_small.txt'):
   with open(file) as f:
     content = [p.strip() for p in f.readlines() if p]
   # packets maps the depth to the current position, the range,
@@ -14,19 +49,15 @@ def packet_scanner(file='input/packets.txt'):
     depth = int(parts[0].strip())
     packets[depth] = [0, int(parts[1].strip()), False]
     max_depth = depth
+  # increment delay until severity becomes 0
+  delay = 0
   severity = 0
-  for d in range(max_depth + 1):
-    if d in packets:
-      r =  packets[d]
-      if r[0] == 0:
-        severity += d * r[1]
-    for rr in packets.values():
-      if rr[2]:
-        rr[0] -=1
-      else:
-        rr[0] += 1
-      if rr[0] == 0 or rr[0] == rr[1] - 1:
-        rr[2] = not rr[2]
-  return severity
+  while True:
+    # print delay
+    if not packet_scan_check(deepcopy(packets), max_depth, delay):
+      break
+    else:
+      delay += 1
+  return delay
 
-print(packet_scanner())
+print(packet_scanner_delay())
