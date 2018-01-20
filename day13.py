fc09811b62a5fb6feb91971837f68172028cb4ef
Alice Chen <alice@perka.com>
2018-01-19 20:31:53 -0500

    Day 13 complete

--- day13.py
+++ day13.py
@@ -1,63 +1,34 @@
 from copy import deepcopy
 """
 http://adventofcode.com/2017/day/13
+* Chinese room theorem
 """
-def __move_packets__(packets):
-  for rr in packets.values():
-    if rr[2]:
-      rr[0] -=1
-    else:
-      rr[0] += 1
-    if rr[0] == 0 or rr[0] == rr[1] - 1:
-      rr[2] = not rr[2]
-  # print packets
-
-def packet_scanner(packets, max_depth):  
+def packet_scan_severity(packets):
   severity = 0
-  for d in range(max_depth + 1):
-    if d in packets:
-      r =  packets[d]
-      if r[0] == 0:
-        severity += d * r[1]
-    __move_packets__(packets)
+  for depth, length in packets.items():
+    if depth % (2 * (length - 1)) == 0:
+      severity += depth * length
   return severity
 
-def packet_scan_check(packets, max_depth, delay=0):
-  for n in range(delay):
-    __move_packets__(packets)
+def packet_scan_check(packets, delay=0):
+  for depth, length in packets.items():
+    if (depth + delay) % (2 * (length - 1)) == 0:
+      return False
+  return True
 
-  caught = False
-  for d in range(max_depth + 1):
-    if d in packets:
-      r =  packets[d]
-      if r[0] == 0:
-        caught = True
-        break
-    __move_packets__(packets)
-  return caught
-
-def packet_scanner_delay(file='input/packets_small.txt'):
+def cross_firewall(file='input/packets.txt'):
   with open(file) as f:
     content = [p.strip() for p in f.readlines() if p]
-  # packets maps the depth to the current position, the range,
-  # and the direction of scanning of each layer 
-  # (False if going down, True if going up)
   packets = {}
-  max_depth = None
   for p in content:
-    parts = p.split(':')
-    depth = int(parts[0].strip())
-    packets[depth] = [0, int(parts[1].strip()), False]
-    max_depth = depth
-  # increment delay until severity becomes 0
+    depth, length = p.split(': ')
+    packets[int(depth)] = int(length)
+  # part 1: calculate severity
+  # return packet_scan_severity(packets)
+  # part 2: increment delay until scan check passes
   delay = 0
-  severity = 0
-  while True:
-    # print delay
-    if not packet_scan_check(deepcopy(packets), max_depth, delay):
-      break
-    else:
-      delay += 1
+  while not packet_scan_check(packets, delay):
+    delay += 1
   return delay
 
-print(packet_scanner_delay())
+print(cross_firewall())
