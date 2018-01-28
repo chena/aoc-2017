a0560ed2cebed14c9d50958b454737c8f6a26874
Alice Chen <alice@perka.com>
2018-01-28 15:29:45 -0500

    day 14 complete

--- day14.py
+++ day14.py
@@ -12,15 +12,41 @@ def square_count():
     total += sum(binary)
   print(total)
 
-def region_coount():
-  used_squares = []
-  # generate matrix
+def __check_neighbors__(i, j, rows, visited):
+  # return early if already visited or cell is zero
+  if (i, j) in visited or not rows[i][j]:
+    return
+  # track visit
+  visited.add((i, j))
+  # recursively check all neighbors
+  if i > 0:
+    __check_neighbors__(i-1, j, rows, visited)
+  if j > 0:
+    __check_neighbors__(i, j-1, rows, visited)
+  if i < 127:
+    __check_neighbors__(i+1, j, rows, visited)
+  if j < 127:
+    __check_neighbors__(i, j+1, rows, visited)
+
+
+def region_count():
+  rows = []
   for h in knot_hashes():
-    binary = [int(d) for d in bin(int(h, 16))[2:]]
-    used_square = [i for i in range(len(binary)) if binary[i] > 0]
-    used_squares.append(used_square)
-  # for i in range()
+    binary = bin(int(h, 16))[2:]
+    binary = '0' * (128 - len(binary)) + binary
+    r = [int(d) for d in binary]
+    rows.append(r)
+  count = 0
+  visited = set()
+  for i in xrange(128):
+    for j in xrange(128):
+      # skip if already visited or cell is zero
+      if (i, j) in visited or not rows[i][j]:
+        continue
+      # increment region
+      count += 1
+      __check_neighbors__(i, j, rows, visited)
+  return count
 
-
-region_coount()
 #square_count()
+print(region_count())
