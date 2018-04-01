b1d110aedf21170fb028318818fdcb7ee9957d70
Alice Chen <alice@perka.com>
2018-04-01 16:44:11 -0400

    day 24 part 1

--- day24.py
+++ day24.py
@@ -18,46 +18,73 @@ class Node:
   def __init__(self, value):
     self.value = value
     self.children = []
-    self.used = False
 
   def add_child(self, child):
-    self.children.append(child)
-
-  def mark_used(self):
-    self.used = True
+    if not child in self.children:
+      self.children.append(child)
 
   def __str__(self):
     return '({}, {})'.format(self.value[0], self.value[1])
 
-def __all_used__(key):
+def __all_used__(key, current_path):
   for c in mapping[key]:
-    if not c.used:
+    if not c in current_path:
       return False
   return True
 
-def __traverse__(node, addup):
-  node.mark_used()
-  key = node.value[1]
-  addup += key + node.value[0]
-  if __all_used__(key):
-    sums.append(addup)
+def __traverse__(node, current_path, current_port):
+  current_path.append(node)
+  f, s = node.value
+  key = f if f != current_port else s
+  if __all_used__(key, current_path):
+    # print 'current path: ', [n.value for n in current_path]
+    sums.append(sum([sum(n.value) for n in current_path]))
     return node
   for c in mapping[key]:
-    if not c.used:
-      node.add_child(__traverse__(c, addup))
+    if not c in current_path:
+      node.add_child(__traverse__(c, current_path[:], key))
   return node
-  
+
+def __print_mapping__():
+  for c in mapping:
+    print '{}: {}'.format(c, [n.value for n in mapping[c]])
+
+def __print_tree__(node):
+  print('----------tree---------')
+  queue = []
+  queue.append(node)
+  while len(queue) > 0:
+    node = queue.pop(0)
+    print(node.value, ' c count: ', len(node.children))
+    for c in node.children:
+      queue.append(c)
+
 def bridges():
   with open('input/bridges.txt') as f:
-    components = [sorted([int(p) for p in comp.split('/')]) for comp in f.readlines()]
+    components = [map(int, comp.split('/')) for comp in f.readlines()]
   for pair in components:
     left, right = pair
     node = Node(pair)
     mapping[left].append(node)
-    mapping[right].append(node)
+    if (left != right):
+      mapping[right].append(node)
   # starts at 0
-  tree = __traverse__(mapping[0][0], 0)
-  # tree2 = __traverse__(mapping[0][1], 0)
+  # zero_node1, zero_node2 = mapping[0]
+  # tree = __traverse__(zero_node1, [], 0)
+  # tree2 = __traverse__(zero_node2, [], 0)
+  tree = __traverse__(mapping[0][0], [], 0)
+  # __print_mapping__()
+  # __print_tree__(tree)
+  # __print_tree__(tree2)
+  # print sums
   return max(sums)
 
 print(bridges())
+# n = Node([0, 0])
+# n1 = Node([1, 1])
+# n2 = Node([2, 2])
+# n3 = Node([3, 3])
+# n.children = [n1, n2]
+# n1.children = [n3, Node([4, 4])]
+# __print_tree__(n)
+
